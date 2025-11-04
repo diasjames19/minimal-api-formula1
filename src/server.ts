6ead7660cc55ef352d64192f22bd6ff79c3d6366
diasjames19 <diasjames@hotmail.com>
2025-11-03 22:45:49 -0300

    api treminada

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,35 @@
 import fastify from "fastify";
+import { request } from "http";
 
 
 
 const server = fastify({logger:true})
-const teams = [{id:1, name:"Ferrrari"}];
+const teams = [{id:1, name:"Ferrrari"} ,{id:2, name:"Maclaren"},{id:3, name:"Mercedes"}];
 
-server.get("/teams", async(request,response)=>{
-    response.type("application/json").code(200);
-    return [{id:1, name:"Ferrrari"}];
+
+interface teamsParams
+{
+    id: string
+}
+server.get("/teams", async(req,res)=>{
+    res.type("application/json").code(200);
+    console.log(teams);
+    return { teams };
+    
+});
+server.get<{Params:teamsParams}>("/teams/:id", async(req,res)=>{
+    res.type("application/json").code(200);
+    const id = parseInt(req.params.id);
+    const team = teams.find((t)=>t.id === id);
+
+    if(!team){
+       res.type("application/json").code(404); 
+       return{message:"Team Não existe!"} 
+    }else{
+        res.type("application/json").code(200);
+        return {team}
+    }
+
 });
 server.listen({port:3333},()=>{
     console.log("Servidor Online!");
